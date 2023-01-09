b4fe84120f154d492d42e5799728a05ff91cc1be
UsefulAd1242 <upalanka.k@gmail.com>
2023-01-09 18:04:02 +0530

    Initial Commit

---
+++ OhMySLT/OhMySLTApp.swift
@@ -0,0 +1,17 @@
+//
+//  OhMySLTApp.swift
+//  OhMySLT
+//
+//  Created by 27k on 2023-01-09.
+//
+
+import SwiftUI
+
+@main
+struct OhMySLTApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
